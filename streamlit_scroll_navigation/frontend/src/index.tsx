9a416120b6f18274de2dd1d915d71f0c53fd8514
SnpM <jpthek9@gmail.com>
2024-10-20 06:02:13 -0600

    fix bad import

--- streamlit_scroll_navigation/frontend/src/index.tsx
+++ streamlit_scroll_navigation/frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from "./ScrollNavigationBar"
 import ScrollNavigationBar from "./ScrollNavigationBar"
 
 ReactDOM.render(
