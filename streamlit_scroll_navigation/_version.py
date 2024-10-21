069f4a0d33f60edccad65c0db2a417c1791759c2
SnpM <jpthek9@gmail.com>
2024-10-20 22:00:18 -0400

    update version

--- streamlit_scroll_navigation/_version.py
+++ streamlit_scroll_navigation/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.3.7.dev0'
-__version_tuple__ = version_tuple = (0, 3, 7, 'dev0')
+__version__ = version = '0.3.11.dev0+gd09c48a.d20241021'
+__version_tuple__ = version_tuple = (0, 3, 11, 'dev0', 'gd09c48a.d20241021')
